f53ad579ab83dac6c93207c8f7f8d374628152c8
Lukas Koeller <acc.koeller@protonmail.com>
2023-05-24 22:53:11 +0200

    chore: init app

---
+++ src/main/java/com/notus/notus/NotusApplication.java
@@ -0,0 +1,12 @@
+package com.notus.notus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NotusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NotusApplication.class, args);
+	}
+}
---
+++ src/test/java/com/notus/notus/NotusApplicationTests.java
@@ -0,0 +1,13 @@
+package com.notus.notus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotusApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
