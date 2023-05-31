af0db9625f45e1599675daec63aa93ba5cf7beaf
Lukas Koeller <acc.koeller@protonmail.com>
2023-05-31 08:13:44 +0200

    chore: init app

---
+++ README.md
@@ -0,0 +1,7 @@
+# notus
+
+This is a simple notes app using markdown.
+
+## References
+
+* <https://spring.io/guides/gs/accessing-data-mysql/>
---
+++ app/postcss.config.cjs
@@ -0,0 +1,25 @@
+const postcssPresetEnv = require("postcss-preset-env");
+const postcssImport = require("postcss-import");
+const combineSelectors = require("postcss-combine-duplicated-selectors");
+
+module.exports = {
+  plugins: [
+    postcssImport(),
+    postcssPresetEnv({
+      stage: 0,
+      autoprefixer: false,
+      features: {
+        "logical-properties-and-values": false,
+        "prefers-color-scheme-query": false,
+        "gap-properties": false,
+        "custom-properties": false,
+        "place-properties": false,
+        "not-pseudo-class": false,
+        "focus-visible-pseudo-class": false,
+        "focus-within-pseudo-class": false,
+        "color-functional-notation": false,
+      },
+    }),
+    combineSelectors(),
+  ],
+};
