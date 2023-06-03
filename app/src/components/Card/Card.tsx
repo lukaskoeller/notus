82d5c2bc44c547eeb2b1d3ef49e18b7b666c6658
Lukas Koeller <acc.koeller@protonmail.com>
2023-06-03 22:00:49 +0200

    feat: add Card component

--- app/src/components/Card/Card.tsx
+++ app/src/components/Card/Card.tsx
@@ -1,17 +1,18 @@
-import { FC } from "react";
+import { HTMLAttributes, FC } from "react";
 import styles from "./Card.module.css"
 
-export type CardProps = {
+export type CardProps = HTMLAttributes<HTMLDivElement> & {
   children: React.ReactNode;
 }
 
 export const Card: FC<CardProps> = (props) => {
   const {
     children,
+    ...rest
   } = props;
 
   return (
-    <div className={styles.card}>
+    <div className={styles.card} {...rest}>
       {children}
     </div>
   );
