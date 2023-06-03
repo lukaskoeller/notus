bdda7fa23da0a73f3f2b4e769eb3acdd983a85e2
Lukas Koeller <acc.koeller@protonmail.com>
2023-06-03 22:01:28 +0200

    feat: add NoteItem

--- app/src/components/NoteItem/NoteItem.tsx
+++ app/src/components/NoteItem/NoteItem.tsx
@@ -4,20 +4,25 @@ import styles from "./NoteItem.module.css";
 export type NoteItemProps = {
   heading: string;
   date: Date | string;
+  preTab?: React.ReactNode;
 }
 
 export const NoteItem: FC<NoteItemProps> = (props) => {
   const {
     heading,
     date,
+    preTab,
   } = props;
 
   return (
     <div className={styles.item}>
-      <h2 className={styles.heading}>{heading}</h2>
-      <span className={styles.info}>
-        {date.toLocaleString([], { day: 'numeric', month: 'short', year: 'numeric' })}
-      </span>
+      <div className={styles.pretab}>{preTab}</div>
+      <div className={styles.main}>
+        <h2 className={styles.heading}>{heading}</h2>
+        <span className={styles.info}>
+          {date.toLocaleString([], { day: 'numeric', month: 'short', year: 'numeric' })}
+        </span>
+      </div>
     </div>
   );
 }
