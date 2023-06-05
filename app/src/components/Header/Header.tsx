dca085159c2a6f0f3edfefdb29a58354c77c1035
Lukas Koeller <acc.koeller@protonmail.com>
2023-06-05 23:02:14 +0200

    chore: add ListIcon and PreviewWriteSwitch

--- app/src/components/Header/Header.tsx
+++ app/src/components/Header/Header.tsx
@@ -1,11 +1,17 @@
 import { FC } from "react";
 import { useSideNav } from "../../hooks/useSideNav";
+import { ListIcon } from "../ListIcon/ListIcon";
+import styles from "./Header.module.css";
+import { PreviewWriteSwitch } from "../PreviewWriteSwitch/PreviewWriteSwitch";
 
 export const Header: FC = () => {
   const { showSideNav } = useSideNav();
   return (
-    <header>
-        <button type="button" onClick={showSideNav}>Open Menu</button>
+    <header className={styles.header}>
+        <button type="button" onClick={showSideNav}>
+          <ListIcon />
+        </button>
+        <PreviewWriteSwitch />
     </header>
   );
 };
