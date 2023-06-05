97ddfe9c9a50f3e03f4de9be447f12b78be3c9bf
Lukas Koeller <acc.koeller@protonmail.com>
2023-06-05 23:02:59 +0200

    chore: add Note entity

---
+++ src/main/java/com/notus/notus/Note.java
@@ -0,0 +1,67 @@
+package com.notus.notus;
+
+import java.sql.Date;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+
+@Entity // This tells Hibernate to make a table out of this class
+public class Note {
+  @Id
+  @GeneratedValue(strategy=GenerationType.AUTO)
+  private Integer id;
+
+  private String title;
+
+  private String src;
+
+  private Date createdAt;
+
+  private Date updatedAt;
+
+  @ManyToOne
+  private User author;
+
+  public Integer getId() {
+    return id;
+  }
+
+  public void setId(Integer id) {
+    this.id = id;
+  }
+
+  public String getTitle() {
+    return title;
+  }
+
+  public void setTitle(String title) {
+    this.title = title;
+  }
+
+  public String getSrc() {
+    return src;
+  }
+
+  public void setSrc(String src) {
+    this.src = src;
+  }
+
+  public String getCreatedAt() {
+    return createdAt;
+  }
+
+  public void setCreatedAt(String createdAt) {
+    this.createdAt = createdAt;
+  }
+
+  public String getUpdatedAt() {
+    return updatedAt;
+  }
+
+  public void setUpdatedAt(String updatedAt) {
+    this.updatedAt = updatedAt;
+  }
+}
