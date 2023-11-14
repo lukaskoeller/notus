b47de50a71a9a3c9c1b68f1ff6e711e9223343cb
Lukas Koeller <acc.koeller@protonmail.com>
2023-11-15 00:46:58 +0100

    chore: add tanstack query

--- app/src/main.tsx
+++ app/src/main.tsx
@@ -3,14 +3,21 @@ import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
 import { SideNavProvider } from './contexts/sidenav.tsx'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { OpenAPI } from './api/index.ts'
 
 // Setting the correct environment based base url for the api.
 OpenAPI.BASE = import.meta.env.VITE_APP_API_URL
 
+// Create a client
+const queryClient = new QueryClient()
+
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <SideNavProvider>
-      <App />
-    </SideNavProvider>
+    <QueryClientProvider client={queryClient}>
+      <SideNavProvider>
+        <App />
+      </SideNavProvider>
+    </QueryClientProvider>
   </React.StrictMode>,
 )
