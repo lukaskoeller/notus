0b26a8ba6e8b3ddd46cce65a1776af5b8dbb0296
Lukas Koeller <acc.koeller@protonmail.com>
2023-11-15 00:46:31 +0100

    chore: set correct OpenAPI base url per env

--- app/src/main.tsx
+++ app/src/main.tsx
@@ -4,6 +4,9 @@ import App from './App.tsx'
 import './index.css'
 import { SideNavProvider } from './contexts/sidenav.tsx'
 
+// Setting the correct environment based base url for the api.
+OpenAPI.BASE = import.meta.env.VITE_APP_API_URL
+
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <SideNavProvider>
