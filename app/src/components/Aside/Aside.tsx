56ee97db36ab422181c8c5acb5563568636d7362
Lukas Koeller <acc.koeller@protonmail.com>
2023-11-15 00:45:42 +0100

    feat: fetch notes from fastapi backend

--- app/src/components/Aside/Aside.tsx
+++ app/src/components/Aside/Aside.tsx
@@ -1,30 +1,31 @@
-import { FC } from "react";
+import { FC, Suspense } from "react";
 import styles from './Aside.module.css';
 import { Card } from "../Card/Card";
 import { NoteItem } from "../NoteItem/NoteItem";
 import { Stack } from "../Stack/Stack";
 import { useSideNav } from "../../hooks/useSideNav";
 import { Search } from "../Search/Search";
+import { useQuery } from "@tanstack/react-query";
+import { DefaultService } from "../../api";
 
 export const Aside: FC = () => {
   const { open, closeSideNav } = useSideNav();
+  const { data: notes } = useQuery({ queryKey: ['notes'], queryFn: async () => await DefaultService.getNotesNotesGet() });
 
   return (
     <aside className={styles.aside} data-open={open}>
       <nav className={styles.nav}>
         <Search />
         <Card style={{ '--card-padding': 0 }}>
-          <Stack>
-            {[
-              '🟢🔵 Application inovex',
-              '🍕 Pizzarezept',
-              '🏋️ Trainingsplan'
-            ].map((item) => {
-              return (
-                <NoteItem heading={item} date={new Date()} />
-                );
-              })}
-          </Stack>
+          <Suspense fallback="Loading…">
+            <Stack>
+              {(notes ?? [])?.map(({ id, title, updated_at }) => {
+                return (
+                  <NoteItem key={id} heading={title} date={new Date(updated_at).toLocaleDateString()} />
+                  );
+                })}
+            </Stack>
+          </Suspense>
         </Card>
         <Card>
           Tags
