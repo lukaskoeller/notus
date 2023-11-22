c2ceba8abb2717aaba888324b06d7be754e7cd98
Lukas Koeller <acc.koeller@protonmail.com>
2023-11-22 23:33:29 +0100

    chore: extend EditorProvider

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,13 +1,16 @@
 import styles from './App.module.css';
 import { Aside } from './components/Aside/Aside';
 import { Main } from './components/Main/Main';
+import { EditorProvider } from './contexts/editor';
 
 function App() {
 
   return (
     <div className={styles.grid}>
-      <Aside />
-      <Main />
+      <EditorProvider>
+        <Aside />
+        <Main />
+      </EditorProvider>
     </div>
   )
 }
--- app/src/components/Main/Main.tsx
+++ app/src/components/Main/Main.tsx
@@ -1,17 +1,14 @@
 import { FC } from "react";
 import styles from "./Main.module.css";
 import { Header } from "../Header/Header";
-import { EditorProvider } from "../../contexts/editor";
 import { Note } from "../Note/Note";
 
 export const Main: FC = () => {
 
   return (
     <main className={styles.main}>
-      <EditorProvider>
-        <Header />
-        <Note />
-      </EditorProvider>
+      <Header />
+      <Note />
     </main>
   )
 };
