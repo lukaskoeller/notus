56d4de09f0a0481433e252e0238752bcb01fbe81
Lukas Koeller <acc.koeller@protonmail.com>
2023-11-22 23:33:47 +0100

    feat: setActive when NoteItem is clicked

--- app/src/components/NoteItem/NoteItem.tsx
+++ app/src/components/NoteItem/NoteItem.tsx
@@ -1,10 +1,12 @@
 import { FC } from "react";
 import styles from "./NoteItem.module.css";
+import { useEditor } from "../../hooks/useEditor";
 
 export type NoteItemProps = {
   heading: string;
   date: Date | string;
   preTab?: React.ReactNode;
+  id: number;
 }
 
 export const NoteItem: FC<NoteItemProps> = (props) => {
@@ -12,10 +14,12 @@ export const NoteItem: FC<NoteItemProps> = (props) => {
     heading,
     date,
     preTab,
+    id,
   } = props;
+  const { setActive } = useEditor();
 
   return (
-    <div className={styles.item}>
+    <button type="button" onClick={() => setActive(id)} className={styles.item}>
       <div className={styles.pretab}>{preTab}</div>
       <div className={styles.main}>
         <h2 className={styles.heading}>{heading}</h2>
@@ -23,6 +27,6 @@ export const NoteItem: FC<NoteItemProps> = (props) => {
           {date.toLocaleString([], { day: 'numeric', month: 'short', year: 'numeric' })}
         </span>
       </div>
-    </div>
+    </button>
   );
 }
