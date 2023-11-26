4c6e0b7fcb5d02da179154c738faf352b18c71ba
Lukas Koeller <acc.koeller@protonmail.com>
2023-11-26 13:49:13 +0100

    feat: add useNote and useUpdateNote

--- app/src/data/index.tsx
+++ app/src/data/index.tsx
@@ -1,4 +1,22 @@
-import { useQuery } from "@tanstack/react-query";
-import { DefaultService } from "../api";
+import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
+import { DefaultService, Note } from "../api";
 
 export const useNotes = () => useQuery({ queryKey: ['notes'], queryFn: async () => await DefaultService.getNotesNotesGet() });
+
+export const useNote = (title: Note["title"] | undefined) => {
+  if (!title) {
+    throw new Error('Could not find note', { cause: `'title' (${title}) is not defined but must be to query the note.` });
+  }
+  return useQuery({ queryKey: ['note', title], queryFn: async () => await DefaultService.getNoteNoteTitleGet(title) });
+}
+
+export const useUpdateNote = (note: Note) => {
+  const queryClient = useQueryClient();
+
+  return useMutation({
+  mutationFn: () => {
+    return DefaultService.updateNoteNoteIdPut(note)
+  },
+  onSettled: () => queryClient.invalidateQueries({ queryKey: ['notes', `note${note.id}`] }),
+});
+};
