567743b04124660d5bee37844cae2945ed8d63ce
Lukas Koeller <acc.koeller@protonmail.com>
2023-11-26 13:48:18 +0100

    chore: make NoteItem a Link

--- app/src/components/NoteItem/NoteItem.tsx
+++ app/src/components/NoteItem/NoteItem.tsx
@@ -1,12 +1,11 @@
 import { FC } from "react";
 import styles from "./NoteItem.module.css";
-import { useEditor } from "../../hooks/useEditor";
+import { Link } from "react-router-dom";
 
 export type NoteItemProps = {
   heading: string;
   date: Date | string;
   preTab?: React.ReactNode;
-  id: number;
 }
 
 export const NoteItem: FC<NoteItemProps> = (props) => {
@@ -14,12 +13,10 @@ export const NoteItem: FC<NoteItemProps> = (props) => {
     heading,
     date,
     preTab,
-    id,
   } = props;
-  const { setActive } = useEditor();
 
   return (
-    <button type="button" onClick={() => setActive(id)} className={styles.item}>
+    <Link to={`/note/${heading}`} className={styles.item}>
       <div className={styles.pretab}>{preTab}</div>
       <div className={styles.main}>
         <h2 className={styles.heading}>{heading}</h2>
@@ -27,6 +24,6 @@ export const NoteItem: FC<NoteItemProps> = (props) => {
           {date.toLocaleString([], { day: 'numeric', month: 'short', year: 'numeric' })}
         </span>
       </div>
-    </button>
+    </Link>
   );
 }
