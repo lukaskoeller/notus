cf813ea66cd7cf75854eafc8fe232cc82baeeff3
Lukas Koeller <acc.koeller@protonmail.com>
2023-11-26 13:47:42 +0100

    chore: query note in MarkDoc

--- app/src/components/MarkDoc/MarkDoc.tsx
+++ app/src/components/MarkDoc/MarkDoc.tsx
@@ -1,12 +1,19 @@
 import Markdoc from "@markdoc/markdoc";
 import React, { FC } from "react";
 import styles from "./MarkDoc.module.css";
-import { useEditor } from "../../hooks/useEditor";
+import { useParams } from "react-router-dom";
+import { useNote } from "../../data";
 
 export const MarkDoc: FC = () => {
-  const { treeNode } = useEditor();
+  const { noteTitle } = useParams<{ noteTitle: string }>();
+  const { data: note, isLoading } = useNote(noteTitle);
+
+  if (isLoading) return null;
+
+  const ast = Markdoc.parse(note?.content);
+  const markdown = Markdoc.transform(ast);
 
   return (
-    <article className={styles.article}>{Markdoc.renderers.react(treeNode, React)}</article>
+    <article className={styles.article}>{Markdoc.renderers.react(markdown, React)}</article>
   );
 };
