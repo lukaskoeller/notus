a47ed03d31fcd7a26523f7d49e25a54da8017c1c
Lukas Koeller <acc.koeller@protonmail.com>
2023-11-26 13:49:37 +0100

    feat: add react router

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,15 +1,31 @@
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
 import styles from './App.module.css';
-import { Aside } from './components/Aside/Aside';
 import { Main } from './components/Main/Main';
 import { EditorProvider } from './contexts/editor';
+import { Root } from "./routes/Root";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Root />,
+    children: [
+      {
+        path: 'note/:noteTitle',
+        element: <Main />,
+      }
+    ]
+  },
+]);
 
 function App() {
 
   return (
     <div className={styles.grid}>
       <EditorProvider>
-        <Aside />
-        <Main />
+        <RouterProvider router={router} />
       </EditorProvider>
     </div>
   )
