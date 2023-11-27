b10cefc0b56682f34942e332abba25e2e49e0fa5
Lukas Koeller <acc.koeller@protonmail.com>
2023-11-27 23:22:43 +0100

    feat: add children index route with Start

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -6,12 +6,17 @@ import styles from './App.module.css';
 import { Main } from './components/Main/Main';
 import { EditorProvider } from './contexts/editor';
 import { Root } from "./routes/Root";
+import { Start } from "./components/Start/Start";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <Root />,
     children: [
+      {
+        path: '/',
+        element: <Start />
+      },
       {
         path: 'note/:noteTitle',
         element: <Main />,
