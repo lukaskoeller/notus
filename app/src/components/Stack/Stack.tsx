c4653d6a02d6d5b5b1b7a3839bf14a54ef9b730c
Lukas Koeller <acc.koeller@protonmail.com>
2023-11-27 23:22:07 +0100

    feat: add gap prop to Stack

--- app/src/components/Stack/Stack.tsx
+++ app/src/components/Stack/Stack.tsx
@@ -3,15 +3,17 @@ import styles from './Stack.module.css';
 
 export type StackProps = {
   children: React.ReactNode;
+  gap?: `var(--${string})`;
 }
 
 export const Stack: FC<StackProps> = (props) => {
   const {
     children,
+    gap,
   } = props;
 
   return (
-    <div className={styles.stack}>
+    <div className={styles.stack} style={{ gap }}>
       {children}
     </div>
   );
