d0c9688fbe3237892db3a268cd528c7d27296d0e
Lukas Koeller <acc.koeller@protonmail.com>
2023-12-03 23:28:51 +0100

    chore: add Editor component

---
+++ app/src/components/Editor/Editor.tsx
@@ -0,0 +1,19 @@
+import { FC } from "react";
+import { MarkDoc } from "../MarkDoc/MarkDoc";
+import { useEditor } from "../../hooks/useEditor";
+import { NoteTextArea } from "../NoteTextArea/NoteTextArea";
+import { Note } from "../../api";
+
+export type EditorProps = Partial<Pick<Note, 'content'>>;
+
+export const Editor: FC<EditorProps> = ({ content = '' }) => {
+  const { mode } = useEditor();
+  console.log(content);
+  
+  
+  return (
+    mode === 'preview'
+      ? <MarkDoc content={content} />
+      : <NoteTextArea content={content} />
+  );
+};
