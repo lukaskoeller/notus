046cd7e1f8075b69790672a6bd4a77d003591222
Lukas Koeller <acc.koeller@protonmail.com>
2023-12-16 21:42:17 +0100

    feat: implement update functionality

--- app/src/components/NoteTextArea/NoteTextArea.tsx
+++ app/src/components/NoteTextArea/NoteTextArea.tsx
@@ -1,17 +1,26 @@
-import { ChangeEvent, FC } from "react";
+import { ChangeEvent, FC, useState } from "react";
 import styles from "./NoteTextArea.module.css";
 import { Note } from "../../api";
+import { useUpdateNote } from "../../data";
+import { useParams } from "react-router-dom";
 
 export type NoteTextAreaProps = Pick<Note, 'content'>;
 
 export const NoteTextArea: FC<NoteTextAreaProps> = ({ content = '' }) => {
+  const [text, setText] = useState(content);
+  const { noteTitle } = useParams<{ noteTitle: string }>();
+  const mutationNote = useUpdateNote();
+  
 
   const handleChange = (e: ChangeEvent) => {
     const value = (e.target as HTMLTextAreaElement).value;
-    // setText(value);
+    setText(value);
+    if (noteTitle) {
+      mutationNote.mutate({ content: value, title: noteTitle });
+    }
   }
 
   return (
-    <textarea className={styles.textarea} value={content} onChange={handleChange} />
+    <textarea className={styles.textarea} value={text} onChange={handleChange} />
   );
 }
