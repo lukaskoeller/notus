910a5eab5b2d40f0169b4036584700660ebc7377
Lukas Koeller <acc.koeller@protonmail.com>
2023-12-16 21:42:36 +0100

    chore: clean file

--- app/src/data/index.tsx
+++ app/src/data/index.tsx
@@ -1,5 +1,5 @@
 import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
-import { DefaultService, Note } from "../api";
+import { DefaultService, Note, UpdateNote } from "../api";
 
 export const useNotes = () => useQuery({ queryKey: ['notes'], queryFn: async () => await DefaultService.getNotesNotesGet() });
 
@@ -7,16 +7,36 @@ export const useNote = (title: Note["title"] | undefined) => {
   if (!title) {
     throw new Error('Could not find note', { cause: `'title' (${title}) is not defined but must be to query the note.` });
   }
-  return useQuery({ queryKey: ['note', title], queryFn: async () => await DefaultService.getNoteNoteTitleGet(title) });
+
+  return useQuery({
+    queryKey: ['note', title],
+    queryFn: () => DefaultService.getNoteNoteTitleGet(title),
+  });
 }
 
-export const useUpdateNote = (note: Note) => {
+export const useUpdateNote = () => {
   const queryClient = useQueryClient();
 
   return useMutation({
-  mutationFn: () => {
-    return DefaultService.updateNoteNoteIdPut(note)
+  mutationFn: (note: UpdateNote & Pick<Note, "title">) => {
+    return DefaultService.updateNoteNoteTitlePatch(note.title, note)
   },
-  onSettled: () => queryClient.invalidateQueries({ queryKey: ['notes', `note${note.id}`] }),
+  onMutate: async (newNote: Note) => {
+    // Cancel any outgoing refetches
+    // (so they don't overwrite our optimistic update)
+    await queryClient.cancelQueries({ queryKey: ['notes', 'note', newNote.title] });
+
+    // Snapshot the previous value
+    const previousNote = queryClient.getQueryData(['note', newNote.title]);
+
+    // Optimistically update to the new value
+    queryClient.setQueryData(['note', newNote.title], newNote)
+
+    return { previousNote, newNote }
+  },
+  onError: (_err, newNote, context) => {
+    queryClient.setQueryData(['note', newNote.title], context?.previousNote)
+  },
+  onSettled: (note) => queryClient.invalidateQueries({ queryKey: ['notes', 'note', note.title] }),
 });
 };
