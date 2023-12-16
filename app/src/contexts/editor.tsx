6c39c2ac0b5f32117ac97b9c6b189c94b328cb82
Lukas Koeller <acc.koeller@protonmail.com>
2023-12-16 23:38:19 +0100

    chore: rm unused code

--- app/src/contexts/editor.tsx
+++ app/src/contexts/editor.tsx
@@ -1,21 +1,13 @@
-import Markdoc, { RenderableTreeNode, Tag } from "@markdoc/markdoc";
-import { Dispatch, FC, SetStateAction, createContext, useEffect, useState } from "react";
-import { useNotes } from "../data";
+import { Dispatch, FC, SetStateAction, createContext, useState } from "react";
 
 export type ViewMode = 'preview' | 'write';
 
 type API = {
   setMode: Dispatch<SetStateAction<ViewMode>>;
-  setText: Dispatch<SetStateAction<string>>;
-  setActive: Dispatch<SetStateAction<number | undefined>>;
 }
 
 type State = {
   mode: ViewMode;
-  text: string;
-  treeNode: RenderableTreeNode;
-  title: string;
-  activeId: number | undefined;
 };
 
 type Context = API & State;
@@ -24,35 +16,9 @@ export const EditorContext = createContext<Context>({} as Context);
 
 export const EditorProvider: FC<{ children: React.ReactNode }> = ({ children }) => {
   const [mode, setMode] = useState<ViewMode>('preview');
-  const [text, setText] = useState<string>('');
-  const [activeId, setActive] = useState<number | undefined>();
-  const [title, setTitle] = useState<string>('');
-  const [treeNode, setTreeNode] = useState<RenderableTreeNode>('');
-  const { data: notes } = useNotes();
-
-  useEffect(() => {
-    const activeNote = (notes ?? []).find((note) => note.id === activeId);
-    
-    if (activeNote?.content) {
-      setText(activeNote?.content)
-    }
-  }, [activeId, notes]);
-
-  useEffect(() => {
-    const ast = Markdoc.parse(text);
-    const content = Markdoc.transform(ast);
-    if (content instanceof Tag) {
-      const firstTag = content?.children?.[0];
-      if (firstTag instanceof Tag && firstTag?.name === 'h1' && typeof firstTag?.children[0] === 'string') {
-        setTitle(firstTag?.children[0])
-      }
-    }
-    setTreeNode(content);
-    
-  }, [text, title]);
 
   return (
-    <EditorContext.Provider value={{ mode, text, setMode, setText, activeId, setActive, treeNode, title }}>
+    <EditorContext.Provider value={{ mode, setMode }}>
       {children}
     </EditorContext.Provider>
   )
