257e9cdaf2da2762188e48251e2879af0099992e
Lukas Koeller <acc.koeller@protonmail.com>
2023-12-20 00:28:59 +0100

    feat: add NewNote component

--- app/src/components/NewNote/NewNote.tsx
+++ app/src/components/NewNote/NewNote.tsx
@@ -1,8 +1,48 @@
-import { FC } from "react";
-import { Note } from "../Note/Note";
+import { FC, FormEvent } from "react";
+import styles from "./NewNote.module.css";
+import { Stack } from "../Stack/Stack";
+import { useCreateNote } from "../../data";
 
 export const NewNote: FC = () => {
+  const createNote = useCreateNote();
+
   return (
-    <Note content="" />
+    <div className={styles.container}>
+      <Stack gap="var(--size-9)">
+        <h1 className="btn">
+          Create note
+        </h1>
+        <form
+          className={styles.form}
+          method="post"
+          onSubmit={(e: FormEvent<HTMLFormElement>) => {
+            e.preventDefault();
+            const form = new FormData(e.target as HTMLFormElement);
+            console.log(form, e);
+            
+            const title = form.get('title') as string;
+
+            createNote.mutate(title);
+          }}
+        >
+          <Stack>
+            <div className={styles.formField}>
+              <label htmlFor="title">Titel</label>
+              <input
+                className={styles.input}
+                type="text"
+                name="title"
+                id="title"
+                autoComplete="hidden"
+                autoFocus
+                required
+                minLength={2}
+              />
+            </div>
+            <button type="submit">Start note</button>
+          </Stack>
+        </form>
+      </Stack>
+    </div>
   );
 }
