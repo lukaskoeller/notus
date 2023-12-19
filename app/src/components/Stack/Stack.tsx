0272f92fda7c701ac64dd9bfb6afcebe30e8459f
Lukas Koeller <acc.koeller@protonmail.com>
2023-12-20 00:29:21 +0100

    chore: allow className prop for Stack

--- app/src/components/Stack/Stack.tsx
+++ app/src/components/Stack/Stack.tsx
@@ -4,16 +4,18 @@ import styles from './Stack.module.css';
 export type StackProps = {
   children: React.ReactNode;
   gap?: `var(--${string})` | '0';
+  className?: React.HTMLAttributes<HTMLDivElement>["className"];
 }
 
 export const Stack: FC<StackProps> = (props) => {
   const {
     children,
     gap,
+    className,
   } = props;
 
   return (
-    <div className={styles.stack} style={{ gap }}>
+    <div className={[styles.stack, className].join(' ')} style={{ gap }}>
       {children}
     </div>
   );
