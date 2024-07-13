1c7034c7edd56e504b36fb3689f653e8b2c923bf
Lukas Koeller <acc.koeller@protonmail.com>
2024-07-14 01:35:35 +0200

    chore: update router

--- app/src/router.ts
+++ app/src/router.ts
@@ -1,27 +1,36 @@
-import { RootRoute, Route } from "@tanstack/react-router";
+import { createRootRoute, createRoute } from "@tanstack/react-router";
 import { Root } from "./routes/Root";
 import { Start } from "./components/Start/Start";
 import { NewNote } from "./components/NewNote/NewNote";
 import { Main } from "./components/Main/Main";
 
-export const rootRoute = new RootRoute({
-    component: Root,
-});
-  
-export const indexRoute = new Route({
-    getParentRoute: () => rootRoute,
-    path: "/",
-    component: Start,
+export const rootRoute = createRootRoute({
+  component: Root,
 });
 
-export const newNoteRoute = new Route({
-    getParentRoute: () => rootRoute,
-    path: "/note",
-    component: NewNote,
+export const indexRoute = createRoute({
+  getParentRoute: () => rootRoute,
+  path: "/",
+  component: Start,
 });
 
-export const noteRoute = new Route({
-    getParentRoute: () => rootRoute,
-    path: "/note/$noteId",
-    component: Main,
+export const newNoteRoute = createRoute({
+  getParentRoute: () => rootRoute,
+  path: "/note",
+  component: NewNote,
+});
+
+export type TNoteSearch = {
+  mode: "view" | "edit";
+};
+
+export const noteRoute = createRoute({
+  getParentRoute: () => rootRoute,
+  path: "/note/$noteId",
+  validateSearch: (search: Record<string, unknown>): TNoteSearch => {
+    return {
+      mode: search.mode === "edit" ? "edit" : "view",
+    };
+  },
+  component: Main,
 });
