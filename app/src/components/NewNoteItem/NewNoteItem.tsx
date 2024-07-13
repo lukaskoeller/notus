2ba27652ee8cb1c14f7c33a67caf83595569686d
Lukas Koeller <acc.koeller@protonmail.com>
2024-07-14 01:37:08 +0200

    chore: simplify NewNoteItem

--- app/src/components/NewNoteItem/NewNoteItem.tsx
+++ app/src/components/NewNoteItem/NewNoteItem.tsx
@@ -1,9 +1,10 @@
 import { FC } from "react";
-import styles from "./NewNoteItem.module.css";
 import { Link } from "@tanstack/react-router";
 
 export const NewNoteItem: FC = () => {
   return (
-    <Link to="/note" className={['btn', styles.button].join(' ')}>Add Note</Link>
+    <Link to="/note" className={"btn"}>
+      Add Note
+    </Link>
   );
-}
+};
