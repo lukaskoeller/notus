62fa3be1accdb9e3d75d255ccdcda44069ac25d9
Lukas Koeller <acc.koeller@protonmail.com>
2024-07-14 20:00:47 +0200

    feat: implement isBackgroundInert state in context

--- app/src/contexts/sidenav.tsx
+++ app/src/contexts/sidenav.tsx
@@ -1,20 +1,47 @@
-import { FC, createContext, useMemo, useState } from "react";
+import { FC, createContext, useEffect, useMemo, useState } from "react";
 
 type API = {
   closeSideNav: () => void;
   showSideNav: () => void;
-}
+};
 
 type State = {
-  open: boolean
+  open: boolean;
+  isBackgroundInert: boolean | undefined;
 };
 
 type Context = API & State;
 
 export const SideNavContext = createContext<Context>({} as Context);
 
-export const SideNavProvider: FC<{ children: React.ReactNode }> = ({ children }) => {
+export const SideNavProvider: FC<{ children: React.ReactNode }> = ({
+  children,
+}) => {
   const [open, setOpen] = useState(false);
+  const [isMediaBelowThreshold, setIsMediaBelowThreshold] = useState<boolean>();
+
+  const isBackgroundInert = isMediaBelowThreshold && open;
+
+  useEffect(() => {
+    const handleMatchMedia = () => {
+      if (window.matchMedia("(max-width: 768px)").matches) {
+        setIsMediaBelowThreshold(true);
+      } else {
+        setIsMediaBelowThreshold(false);
+      }
+    };
+    
+    if (isMediaBelowThreshold === undefined) {
+      handleMatchMedia();
+    }
+
+    window.addEventListener("resize", handleMatchMedia);
+
+    return () => {
+      window.removeEventListener("resize", handleMatchMedia);
+    };
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
 
   const api = useMemo(() => {
     const closeSideNav = () => setOpen(false);
@@ -23,12 +50,12 @@ export const SideNavProvider: FC<{ children: React.ReactNode }> = ({ children })
     return {
       closeSideNav,
       showSideNav,
-    }
+    };
   }, []);
 
   return (
-    <SideNavContext.Provider value={{ open, ...api }}>
+    <SideNavContext.Provider value={{ open, isBackgroundInert, ...api }}>
       {children}
     </SideNavContext.Provider>
-  )
+  );
 };
