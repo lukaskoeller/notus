585cd5a598af3df1a89d3ecc65010d677084fc00
Lukas Koeller <acc.koeller@protonmail.com>
2024-07-15 23:27:35 +0200

    chore: update imports

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,10 +1,10 @@
 import styles from './App.module.css';
-import { EditorProvider } from './contexts/editor';
+import { EditorProvider } from './editorContext/editor';
 import { createRouter, RouterProvider } from "@tanstack/react-router";
 import { indexRoute, newNoteRoute, noteRoute, rootRoute } from './router';
 import { OpenAPI } from './api';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import { SideNavProvider } from './contexts/sidenav';
+import { SideNavProvider } from './sideNavContext/sideNav';
 
 const apiUrl = import.meta.env.VITE_APP_API_URL;
 
