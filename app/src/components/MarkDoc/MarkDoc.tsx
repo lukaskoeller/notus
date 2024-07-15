52324267bef847445e7d5d00baccb36f6a9e1dd7
Lukas Koeller <acc.koeller@protonmail.com>
2024-07-15 23:25:51 +0200

    chore: simplify MarkDoc again

--- app/src/components/MarkDoc/MarkDoc.tsx
+++ app/src/components/MarkDoc/MarkDoc.tsx
@@ -4,18 +4,13 @@ import styles from "./MarkDoc.module.css";
 import { useApiReadNote } from "../../data";
 
 export const MarkDoc: FC = () => {
-  const { data: note, isLoading, isSuccess } = useApiReadNote();
-  
-  if (isLoading) return null;
+  const { data: note } = useApiReadNote();
+  const ast = Markdoc.parse(note?.content ?? "");
+  const markdown = Markdoc.transform(ast);
 
-  if (isSuccess) {
-    const ast = Markdoc.parse(note.content);
-    const markdown = Markdoc.transform(ast);
-  
-    return (
-      <article className={styles.article}>{Markdoc.renderers.react(markdown, React)}</article>
-    );
-  }
-
-  return null;
-}
+  return (
+    <article className={styles.article}>
+      {Markdoc.renderers.react(markdown, React)}
+    </article>
+  );
+};
