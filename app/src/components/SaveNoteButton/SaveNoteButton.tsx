bc4614cda1114f3800657e887e19c9fe7b31c3c2
Lukas Koeller <acc.koeller@protonmail.com>
2024-07-18 22:30:36 +0200

    chore: Comment out unused code in SaveNoteButton component

--- app/src/components/SaveNoteButton/SaveNoteButton.tsx
+++ app/src/components/SaveNoteButton/SaveNoteButton.tsx
@@ -1,8 +1,8 @@
 import { FC } from "react";
-import { useUpdateNote } from "../../shared/useUpdateNote";
 
 export const SaveNoteButton: FC = () => {
-    const { updateNote } = useUpdateNote();
+    // const { noteId } = noteRoute.useParams();
+    // const { updateNote } = useUpdateNote();
     return (
         <button type="button" className="btn">Save</button>
     )
